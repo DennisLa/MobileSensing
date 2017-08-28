1ba564029ab922c82fbf90b1f6015894c9f52776
DennisLa <dennis.lawo@web.de>
2017-08-28 10:52:43 +0200

    TrackRecognition

---
+++ mobilesensing_module/src/main/java/de/dennis/mobilesensing_module/mobilesensing/Sensors/OtherSensors/LiveTrackRecognition/LiveTrackRecognitionListener.java
@@ -0,0 +1,8 @@
+package de.dennis.mobilesensing_module.mobilesensing.Sensors.OtherSensors.LiveTrackRecognition;
+
+/**
+ * Created by Dennis on 28.08.2017.
+ */
+
+public class LiveTrackRecognitionListener {
+}
